ce4997dcd8094a0705879499f90e323d82d8a2ee
Agustin-desktop <agustin2051@gmail.com>
2024-07-28 21:20:23 -0300

    Various

--- prisma/migrations/20240729002013_init/migration.sql
+++ prisma/migrations/20240729002013_init/migration.sql
@@ -1,26 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `CelestialBody` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Observation` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "Observation" DROP CONSTRAINT "Observation_celestialBodyId_fkey";
-
--- DropForeignKey
-ALTER TABLE "Observation" DROP CONSTRAINT "Observation_userId_fkey";
-
--- DropTable
-DROP TABLE "CelestialBody";
-
--- DropTable
-DROP TABLE "Observation";
-
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "user" (
     "id" TEXT NOT NULL,
